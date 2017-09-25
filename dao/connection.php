0ea74092e639668f269d0a727f01d79569944133
Franck <fhakizimana@bluesquare.org>
2017-09-25 13:12:51 +0200

    remove password

--- dao/connection.php
+++ dao/connection.php
@@ -2,7 +2,7 @@
 	class Connection{
 		private $chaineConnection;
 		public function __construct(){
-			$this->chaineConnection=new PDO("mysql:host=localhost;dbname=stock","root","Francesco@361");
+			$this->chaineConnection=new PDO("mysql:host=localhost;dbname=stock","root","");
 		}
 		public function connectiondb(){
 			return $this->chaineConnection;
